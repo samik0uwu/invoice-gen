cb69305057b2c0c63c76577d016ec4212d53fe86
sam <sam.brchel@gmail.com>
2023-09-20 15:50:49 +0200

    just gui todo

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 ---
 ## TO-DO:
 ### GUI
-    - tkinter
-    - textbox, buttons, combobox, filedialog
-    - make it pretty pls its so ugly rn :c
+- tkinter
+- ~~textbox, buttons, combobox, filedialog~~
+> - path_text make readonly, change size cuz looks wonky rn
+> - make all the elements a bit bigger and align them 
+> - browse_btn,combobox and export_btn same height as path_text
+> - combobox first value set on start so its not empty
+> - output_text make readonly, change size cuz its UGLY
+> - add progressbar above output_text, columnspan=2, indeterminate
+> - change font at least for textboxes
+
 ### Input
     - load file
     - get relevant columns
--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import filedialog as fd
 
-def callback():
+def callback(): #rename from callback bcuz then im gonna have to save the file too in a different method
     name = fd.askopenfilename()
     path_text.insert('end', name)
 
@@ -11,17 +11,20 @@ root = Tk()
 root.geometry('350x200')
 root.title(':3')
 
-path_text = Text(root, height = 2, width = 20)
+path_text = Text(root, height = 2, width = 20) #make readonly
 browse_btn = Button(root, text="Browse", command=callback)
 
 #https://www.pythontutorial.net/tkinter/tkinter-combobox/
 current_var = StringVar()
-combobox = Combobox(root, textvariable=current_var)
+combobox = Combobox(root, textvariable=current_var) #make first value set by default
 combobox['values']=('VN', 'NN')
 combobox['state'] = 'readonly'
 
 export_btn = Button(root, text="Export")
-output_text = Text(root, height = 4, width = 40)
+
+#https://www.pythontutorial.net/tkinter/tkinter-progressbar/
+
+output_text = Text(root, height = 4, width = 40) #also make readonly - will be used for errors or sth maybe it might not be necessarry 
 
 
 
@@ -30,6 +33,6 @@ browse_btn.grid(column=1, row=0)
 combobox.grid(column=0, row=1)
 export_btn.grid(column=1, row=1)
 output_text.grid(column=0, row=2, columnspan=2, padx=10, pady=10)
-#columnspan=2 u output textboxu
+
 
 root.mainloop()
