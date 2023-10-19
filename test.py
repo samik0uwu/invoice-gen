c583544ecd33c4863198f1a1fdcc730fb9d1278c
sam <sam.brchel@gmail.com>
2023-10-19 15:00:50 +0200

    second col sums progress

--- test.py
+++ test.py
@@ -16,17 +16,30 @@ data =db.ws(ws='Sheet1').col(col=1)
 #     print(item[1])
 
 
-secondCol = [112,71,77,87,95]
+secondCol = [112,71,77,88,95]
 
 dbs = [];
 for i,item in enumerate(secondCol, start=0):
     dbs.append(db.ws(ws='Sheet1').col(col=item))
 
-for i, item in enumerate(dbs, start=0):
-    print(i)
-    print(item)
-    print("aaaaaaaaaaaaaa")
+# for i, item in enumerate(dbs[0], start=0):
+#     print(i)
+#     print(item)
+#     print("aaaaaaaaaaaaaa")
 
-print(":pppppp")
-print(len(dbs[0]))
+for i in range(len(dbs[0])):
+    x=0
+    print("start")
+    for j in range(len(dbs)):
+        if (isinstance(dbs[j][i], float) or isinstance(dbs[j][i], int)):
+            x+=dbs[j][i]
+            print("{:.2f}".format(x))
+        else:
+            print("its not float its "+str(type(dbs[j][i])))
+
+    print("end")
+         
+    
+    print("{:.2f}".format(x))
+
 
